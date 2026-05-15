6aa654c8292e2bee792c8f44ed710dadcd9745de
habil <habilherdianto233z@gmail.com>
2026-05-15 13:25:55 +0700

    increase limit

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -30,7 +30,7 @@ export const getAllProducts = async (req:Request, res:Response) => {
         const {search, minPrice, sortBy} = req.query
 
         const page = Number(req.query.page) || 1
-        const limit = Number(req.query.limit) || 5
+        const limit = Number(req.query.limit) || 10
 
         const skip = (page -1) * limit
 
