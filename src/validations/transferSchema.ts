f95d9c793dfb73a2dfb918e5c451e2c3044c0179
habil <habilherdianto233z@gmail.com>
2026-05-18 16:21:09 +0700

    change transfer requirement

--- src/validations/transferSchema.ts
+++ src/validations/transferSchema.ts
@@ -3,5 +3,5 @@ import {z} from "zod";
 export const transferPointSchema = z.object({
     senderId: z.coerce.number("Harus angka"),
     receiverId: z.coerce.number("Harus angka"),
-    amount: z.coerce.number("Harus angka")
+    amount: z.coerce.number().positive("Harus angka dan lebil dari 0")
 })
