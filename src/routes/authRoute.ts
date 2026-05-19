e4291058f54f1c74190d6af96035f74bafd2e14e
habil <habilherdianto233z@gmail.com>
2026-05-19 15:04:06 +0700

    add validation on login

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { register, login } from "../controllers/authController";
 import { upload } from "../lib/multer";
+import { validate } from "../middlewares/validate";
+import { loginSchema } from "../validations/loginSchema";
 
 const router = Router()
 
 router.post("/register", upload.single("image"), register)
-router.post("/login", login)
+router.post("/login", validate(loginSchema) , login)
 
 export default router
