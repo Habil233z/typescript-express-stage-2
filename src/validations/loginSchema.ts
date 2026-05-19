a690199fe48241eb4a9f3dc692e90dac297348ae
habil <habilherdianto233z@gmail.com>
2026-05-19 15:16:32 +0700

    minor name change

--- src/validations/loginSchema.ts
+++ src/validations/loginSchema.ts
@@ -1,6 +1,6 @@
 import {z} from "zod"
 
 export const loginSchema = z.object({
-    email: z.string().min(3, "Nama user minimal 3 karakter"),
+    email: z.string().min(3, "Email user minimal 3 karakter"),
     password: z.string().min(8, "Password minimal 8 karakter"),
 })
