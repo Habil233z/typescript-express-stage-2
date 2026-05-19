80e21dd166f4c1fcf35bacfdecb803d07e2ecd4c
habil <habilherdianto233z@gmail.com>
2026-05-19 13:26:52 +0700

    connect register schema

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { register, login } from "../controllers/authController";
 import { upload } from "../lib/multer";
+import { validate } from "../middlewares/validate";
+import { registerSchema } from "../validations/registerSchema";
 
 const router = Router()
 
-router.post("/register", upload.single("image"), register)
+router.post("/register", validate(registerSchema),upload.single("image"), register)
 router.post("/login", login)
 
 export default router
